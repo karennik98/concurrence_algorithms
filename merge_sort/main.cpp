45827dc4eb3959b931e9ba503c69d8af9b068043
karennik98 <karen.nikoghosyan.98@gmail.com>
2019-11-04 19:36:19 +0400

    Merge sort

---
+++ merge_sort/main.cpp
@@ -0,0 +1,18 @@
+#include "marge_sort.hpp"
+
+#include <iostream>
+
+int main()
+{
+    std::vector<int> vec{4,9,7,3,1,9,9,14,5,6,4};
+
+    merge_sort<int>(vec, 0, vec.size());
+    
+    for(const auto& el : vec)
+    {
+        std::cout<<el<<std::ends;
+    }
+    std::cout<<std::endl;
+
+    return 0;
+}
---
+++ merge_sort/merge_sort.hpp
@@ -0,0 +1,52 @@
+#pragma onec
+
+#include <thread>
+#include <vector>
+
+template<typename T>
+void merge(std::vector<T>& vec, size_t start, size_t mid, size_t end)
+{
+    std::vector<T> one (vec.begin() + start, vec.begin() + mid + 1);
+    std::vector<T> two (vec.begin() + mid + 1, vec.begin() + end + 1);
+
+    size_t i = 0;
+    size_t j = 0;
+    size_t index = start;
+    
+    while (i < one.size() && j < two.size())
+    {
+        if (one[i] < two[j])
+            vec[index ++] = one[i ++];
+        else 
+            vec[index ++] = two[j ++];
+    }
+
+    while (i < one.size())
+        vec[index ++] = one[i ++];
+    while (j < two.size())
+        vec[index ++] = two[j ++];
+}
+
+template<typename T>
+void merge_sort(std::vector<T>& vec, size_t start, size_t end)
+{
+    if (start >= end)
+        return;
+
+    size_t mid = start + (end - start) / 2;
+
+    std::thread first(merge_sort<T>, std::ref(vec), start, mid);
+    std::thread second(merge_sort<T>, std::ref(vec), mid + 1, end);
+
+    first.join();
+    second.join();
+
+    
+    // single-std::thread version, testified.
+    // merge_sort<T>(vec, start, mid);
+    // merge_sort<T>(vec, mid + 1, end); 
+    
+
+    merge(vec, start, mid, end);
+}
+
