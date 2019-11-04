f141c5548fde44522fad567d92ebfbc56b248683
karennik98 <karen.nikoghosyan.98@gmail.com>
2019-11-05 00:36:47 +0400

    Small fix

--- fibonacci/fibonacci.hpp
+++ fibonacci/fibonacci.hpp
@@ -11,10 +11,14 @@ int64_t concurrent_fibonacci(int64_t number )
     if(number == 1 || number == 2)
         return 1;
 
-    int64_t el_1 = std::async(concurrent_fibonacci, number-1).get();
-    int64_t el_2 = concurrent_fibonacci(number-2);
+    std::future<int64_t> future_1 = std::async(concurrent_fibonacci, number-1);
+    //maybe second thread useless
+    std::future<int64_t> future_2 = std::async(concurrent_fibonacci, number-2);
 
-    return el_1 + el_2;
+    future_1.wait();
+    future_2.wait();
+
+    return future_1.get() + future_2.get();
 }
 
 int64_t single_thread_fibonacci(int64_t number )
